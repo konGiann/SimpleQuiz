2ebb866b8313b7bd496380519c58c22840f9d8d9
Konstantinos Giannopoulos <kostaswipe@gmail.com>
2020-04-08 11:10:51 +0300

    Added volume control slider

--- SimpleQuiz/Assets/Scripts/Question.cs
+++ SimpleQuiz/Assets/Scripts/Question.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [Serializable]
 public class Question {
 
     public string Text;
-
+    
     public Sprite Image;
 
     public Answer[] Answers;       
---
+++ SimpleQuiz/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    float volume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = SoundManager.sm.audioController;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = volume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        volume = vol;
+    }
+}
